740fe8b31ebd186001c036fa6b1daee0149c3a24
Yogendra Yadav <yogendra@e9ine.com>
2019-10-25 14:18:47 +0100

    fix: discount modal text edit

--- src/webapp/common/modals/CouponsModal.vue
+++ src/webapp/common/modals/CouponsModal.vue
@@ -1,7 +1,11 @@
 <template>
     <b-modal v-model="showCoupons" centered hide-header hide-footer no-close-on-esc no-close-on-backdrop>
         <div class="coupons-wrapper">
-            <h3 class="section-title-2 mb24 pos-relative">Discount Coupons <a @click="close"><i class="material-icons pos-absolute pos-right brand-secondary light">close</i></a></h3>
+            <h3 class="section-title-2 mb24 pos-relative">Discount Coupons
+                <a @click="close">
+                    <i class="material-icons pos-absolute pos-right brand-secondary light">close</i>
+                </a>
+            </h3>
             <div class="form-group">
                 <input type="text" class="form-control" v-model="couponCode" placeholder="Enter coupon code">
                 <button class="btn btn-primary pos-absolute pos-right pos-top" :disabled="!couponCode" @click="applyCoupon(undefined)">Apply Coupon</button>
@@ -11,9 +15,9 @@
                     <p class="mb0">{{ coupon.CouponCode }}</p>
                 </div>
                 <h6 class="hero-text mb8">{{ coupon.Name }}</h6>
-                
+
                 <p class="lead t-s mb0">{{ coupon.Description }}</p>
-                <p class="t-xs  text-muted mb16">Use code {{ coupon.CouponCode }} & get 60% discount on the base subscription amount.</p>
+                <p class="t-xs  text-muted mb16">Use code {{ coupon.CouponCode }} & get discount on the base subscription amount.</p>
                 <button class="btn btn-primary btn-bordered" @click="applyCoupon(coupon.CouponCode)" :disabled="isLoading">Apply Coupon</button>
             </div>
         </div>
@@ -22,7 +26,7 @@
 
 <script>
 import instance from '@/api';
-import {mapGetters} from 'vuex';
+import { mapGetters } from 'vuex';
 export default {
     name: 'CouponsModal',
     props: ['showCoupons', 'options'],
@@ -78,22 +82,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    .coupons-wrapper {
-        background: $white;
+.coupons-wrapper {
+    background: $white;
+    padding: 16px;
+    .coupons {
+        border: 1px solid #ddd;
         padding: 16px;
-        .coupons {
+        margin-bottom: 24px;
+        .coupon-code {
+            display: inline-block;
+            padding: 8px 16px;
+            background: #fffae6;
             border: 1px solid #ddd;
-            padding: 16px;
-            margin-bottom: 24px;
-            .coupon-code {
-                display: inline-block;
-                padding: 8px 16px;
-                background: #fffae6;
-                border: 1px solid #ddd;
-                p {
-                    letter-spacing: 1px;
-                }
+            p {
+                letter-spacing: 1px;
             }
         }
     }
+}
 </style>
