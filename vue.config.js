56ee5b401994393610521743f6be1310284cd41e
Pratik Kanthi <pratik.kanthi@e9ine.com>
2020-05-11 17:39:17 +0530

    feat: added PWA

--- vue.config.js
+++ vue.config.js
@@ -15,5 +15,26 @@ module.exports = {
             }
         }
     },
-    lintOnSave: false
+    lintOnSave: false,
+    pwa: {
+        name: 'LocalTV Ads',
+        themeColor: '#ff6500',
+        msTileColor: '#ff6500',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'black-translucent',
+        workboxPluginMode: 'InjectManifest',
+        workboxOptions: {
+            exclude: [/\.map$/, /manifest\.json$/],
+            include: [/.*index.html/, /.*\.js/, /.*\.css/, /.*\.png|gif|jpg|svg/],
+            swSrc: 'src/service-worker.js',
+            maximumFileSizeToCacheInBytes: 30097152
+        },
+        iconPaths: {
+            favicon32: 'img/icons/favicon-32x32.png',
+            favicon16: 'img/icons/favicon-16x16.png',
+            appleTouchIcon: 'img/icons/apple-touch-icon.png',
+            maskIcon: 'img/icons/safari-pinned-tab.svg',
+            msTileImage: 'img/icons/mstile-150x150.png'
+        }
+    }
 };
