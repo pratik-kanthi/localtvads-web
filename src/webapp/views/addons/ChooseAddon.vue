a5c8096d77e363637f43ab14c7b6793567b7ae8b
Pratik Kanthi <pratik.kanthi@e9ine.com>
2020-07-20 23:29:44 +0530

    feat: updated booking flow CSS for mobile-view

--- src/webapp/views/addons/ChooseAddon.vue
+++ src/webapp/views/addons/ChooseAddon.vue
@@ -1,48 +1,46 @@
 <template>
-    <div class="addons-parent">
-        <div class="container">
-            <div class="mt64 brand-primary pointer" @click="goBack">Go Back</div>
-            <h3 class="mt64 brand-secondary">Step 2 : Choose AddOns</h3>
-            <div class="mt24 t-xl">Do you have your own ad as a video file? If not, our team of specialists will create one for you</div>
+    <div class="container">
+        <h3 class="mt64 page-heading">Step 2 : Choose AddOns</h3>
+        <div class="mt24 t-xl">Do you have your own ad as a video file? If not, our team of specialists will create one for you</div>
 
-            <b-form-group class="mt48">
-                <b-form-radio class="t-xl" v-model="isAddOn" value="clientad">I have my own ad.</b-form-radio>
-                <b-form-radio class="t-xl mt32" v-model="isAddOn" value="addon">I don't have an ad.</b-form-radio>
-            </b-form-group>
+        <b-form-group class="mt48">
+            <b-form-radio class="t-xl" v-model="isAddOn" value="clientad">I have my own ad.</b-form-radio>
+            <b-form-radio class="t-xl mt32" v-model="isAddOn" value="addon">I don't have an ad.</b-form-radio>
+        </b-form-group>
 
-            <div v-if="isAddOn == 'addon'">
-                <h4 class="brand-secondary mt64 mb48">Select Your Add On</h4>
-                <div class="row addons-wrapper mb24">
-                    <div class="col-lg-6 mb16" v-for="addon in addons" :key="addon._key">
-                        <div class="addon" :class="{ 'active-addon': $parent.serviceAddOn._id === addon._id }">
-                            <div class="name">
-                                <h5>{{ addon.Name }}</h5>
-                                <p class="desc">{{ addon.Description }}</p>
-                            </div>
-                            <div class="price">
-                                <p class="lead brand-primary mb0">Now for just</p>
-                                <h4 class="amount">{{ addon.TotalAmount | currency }}</h4>
-                            </div>
-                            <div class="benefits">
-                                <ul class="mb8">
-                                    <li v-for="benefit in addon.Benefits" :key="benefit">{{ benefit }}</li>
-                                </ul>
-                            </div>
-                            <div class="selectaddon">
-                                <button class="btn btn-primary btn-full" @click="selectAddon(addon)" :class="{ 'btn-selected': $parent.serviceAddOn._id === addon._id }">
-                                    <span v-if="$parent.serviceAddOn._id === addon._id">Selected</span>
-                                    <span v-else>Choose this Addon</span>
-                                </button>
-                            </div>
+        <div v-if="isAddOn == 'addon'">
+            <h4 class="brand-secondary mt64 mb48">Select Your Add On</h4>
+
+            <div class="addons-wrapper mb24">
+                <div class="addon-container" v-for="addon in addons" :key="addon._key">
+                    <div class="addon" :class="{ 'active-addon': $parent.serviceAddOn._id === addon._id }">
+                        <div class="name">
+                            <h5>{{ addon.Name }}</h5>
+                            <p class="desc">{{ addon.Description }}</p>
+                        </div>
+                        <div class="price">
+                            <p class="lead brand-primary mb0">Now for just</p>
+                            <h4 class="amount">{{ addon.TotalAmount | currency }}</h4>
+                        </div>
+                        <div class="benefits">
+                            <ul class="mb8">
+                                <li v-for="benefit in addon.Benefits" :key="benefit">{{ benefit }}</li>
+                            </ul>
+                        </div>
+                        <div class="selectaddon">
+                            <button class="btn btn-primary btn-full" @click="selectAddon(addon)" :class="{ 'btn-selected': $parent.serviceAddOn._id === addon._id }">
+                                <span v-if="$parent.serviceAddOn._id === addon._id">Selected</span>
+                                <span v-else>Choose this Addon</span>
+                            </button>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
 
-            <div class="action mt48 mb64 d-flex justify-content-center">
-                <button class="btn btn-white w-25 border" @click="cancel">Cancel</button>
-                <button class="btn btn-primary w-25 ml16" @click="saveAddOnOption">Proceed</button>
-            </div>
+        <div class="action mt48 mb64 d-flex justify-content-center">
+            <button class="btn btn-white w-25 border" @click="cancel">Cancel</button>
+            <button class="btn btn-primary w-25 ml16" @click="saveAddOnOption">Proceed</button>
         </div>
     </div>
 </template>
@@ -101,6 +99,113 @@ export default {
     }
 };
 </script>
+
+<style lang="scss" scoped>
+.addons-wrapper {
+    display: flex;
+    overflow-x: auto;
+
+    .addon-container {
+        min-width: 280px;
+        margin: 8px;
+
+        &::-webkit-scrollbar {
+            display: none;
+        }
+
+        .addon {
+            background-color: $white;
+            border: 1px solid #ddd;
+            border-radius: 6px;
+            text-align: center;
+
+            &.active-addon {
+                box-shadow: 0 0 8px 0 rgba(255, 101, 0, 0.5);
+                border: 1px solid $brand-primary;
+            }
+
+            .name {
+                padding: 12px 16px;
+                border-bottom: 1px solid #ddd;
+
+                h5 {
+                    font-weight: 500;
+                    font-size: 24px;
+                    color: $brand-primary;
+                    margin-bottom: 4px;
+                }
+
+                .desc {
+                    font-size: 16px;
+                    font-family: $font-family-heading;
+                    font-weight: 400;
+                    margin-bottom: 0;
+                    opacity: 0.6;
+                }
+            }
+
+            .price {
+                padding: 12px 16px;
+                border-bottom: 1px solid #ddd;
+
+                .amount {
+                    font-weight: 500;
+                    font-size: 36px;
+                    margin-bottom: 4px;
+                    color: $brand-secondary;
+                }
+            }
+
+            .benefits {
+                padding: 16px 36px;
+
+                ul {
+                    @include list-unstyled();
+
+                    li {
+                        font-family: $font-family-heading;
+                        font-size: 14px;
+                        color: $base;
+                        text-align: left;
+                        margin-bottom: 16px;
+                        line-height: 20px;
+                        cursor: pointer !important;
+                        padding-left: 32px;
+                        position: relative;
+
+                        &:last-child {
+                            margin-bottom: 0;
+                        }
+
+                        &:before {
+                            content: '';
+                            background-image: url('../../../assets/images/tick.svg');
+                            height: 16px;
+                            width: 16px;
+                            left: 0;
+                            top: 2px;
+                            background-size: cover;
+                            position: absolute;
+                            background-repeat: no-repeat;
+                        }
+                    }
+                }
+            }
+
+            .selectaddon {
+                padding: 0 32px 24px;
+                opacity: 1;
+                z-index: 2;
+
+                .btn-selected {
+                    background-color: $brand-secondary !important;
+                    border: none;
+                }
+            }
+        }
+    }
+}
+</style>
 
 <style lang="scss">
 .custom-radio {
