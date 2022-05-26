ddcae6195da2a6a7e733888f3d8909137a14b60e
Pratik Kanthi <pratik.kanthi@e9ine.com>
2022-05-26 13:41:58 +0100

    feat: updated apis

---
+++ src/config.js
@@ -0,0 +1,12 @@
+window.endpoint = 'https://localtvads-api.herokuapp.com';
+window.socketendpoint = 'https://localtvads-api.herokuapp.com:9997';
+window.googleCredentials = {
+    apiKey: '20k8iF8V4pJnjAwrhQqexEq2',
+    clientId: '955842204870-l42aal4di5env4ud2t31m4ici46l70lf.apps.googleusercontent.com'
+};
+window.facebook = {
+    appId: '639711399851969'
+};
+window.bucketendpoint = 'https://storage.googleapis.com/localtvads-dev-bucket/';
+window.stripeKey = 'pk_test_tvB0M3nfDwXeHv7heAxjEYTc';
+window.slotduration = 175;
